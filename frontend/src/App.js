16ff66e192c6eaa806eca2271120caec25653b7f
nmreddy1911 <manishreddy2000n@gmail.com>
2020-12-27 14:17:49 +0530

    code-mirror

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import { UnControlled as CodeMirror } from "react-codemirror2";
+require("codemirror/lib/codemirror.css");
+require("codemirror/theme/material.css");
+require("codemirror/theme/neat.css");
+require("codemirror/mode/xml/xml.js");
+require("codemirror/mode/javascript/javascript.js");
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <CodeMirror
+        value="console.log('hello')"
+        options={{
+          mode: "javascript",
+          theme: "material",
+          lineNumbers: true,
+        }}
+        onChange={(editor, data, value) => {}}
+      />
     </div>
   );
 }
